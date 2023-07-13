73539d6ea90cc297a66c758f0a9675fe66e62f1d
Nicholas Kasprak <nkasprak@gmail.com>
2023-07-13 16:42:05 -0400

    change picker to buttons

--- node/src/data_manager.js
+++ node/src/data_manager.js
@@ -13,13 +13,21 @@ const DataManager = function() {
     return active_col
   }
 
-  var picker = document.querySelectorAll("#" + id + " select[name='data-picker']")[0]
+  var picker = document.querySelectorAll("#" + id + " div.data-picker")[0]
 
-  active_col = picker.value
+  active_col = picker.querySelectorAll(".picker-option.selected")[0].getAttribute("data-value")
 
-  picker.addEventListener("change", () => {
-    active_col = picker.value
-    change_handler(active_col)
+  var options = picker.querySelectorAll(".picker-option")
+
+  options.forEach((option) => {
+    option.addEventListener("click", () => {
+      options.forEach((_option) => {
+        _option.classList.remove("selected")
+      })
+      option.classList.add("selected")
+      active_col = option.getAttribute("data-value")
+      change_handler(active_col)
+    })
   })
 
 }
